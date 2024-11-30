8e638da105884ec15f454707962a23446ef9e6ed
Ahmed <ahmedmido12398@outlook.com>
2024-11-30 05:05:56 +0200

    Version 3

--- Calculations.js
+++ Calculations.js
@@ -57,4 +57,85 @@ for (let i = 0; i < 3; i++) {
   materials[6][i] = abidosHunting[i];
 }
 
+const conversion = Array(7).fill(false)
+
+for (let i = 0; i < 7 ; i++){
+  const rare = materials[i][0];
+  const uncommon = materials[i][1];
+  conversion[i] = greenTOBlue(uncommon,rare)
+}
+
+
+const orehaRevenue = revenue(Oreha,true)
+const abidosRevenue = revenue(Abidos,false)
+
+
+const craftingPrice = Array(7).fill(0)
+
+for (let i = 0 ; i < 7 ; i++){
+  if (i == 0)
+  {
+      //craftingPrice[i] = oreha(materials[i, 0], materials[i, 1], materials[i, 2], true, false, conversion[i]);
+      craftingPrice[i] = oreha(materials[i, 0], materials[i, 1], materials[i, 2], false, conversion[i], false);
+  }
+  else if (i == 1)
+  {
+      //craftingPrice[i] = oreha(materials[i, 0], materials[i, 1], materials[i, 2], false, conversion[i], false);
+      craftingPrice[i] = oreha(materials[i, 0], materials[i, 1], materials[i, 2], true, false, conversion[i]);
+  }
+  else
+  {
+      craftingPrice[i] = abidos(materials[i, 0], materials[i, 1], materials[i, 2], conversion[i]);
+  }
+}
+
+console.log("Fishing Oreha:");
+profit(craftingPrice[0], orehaRevenue);
+if (conversion[0]) {
+  console.log("Conversion Green to Blue is Better");
+}
+console.log("");
+
+console.log("Excavating Oreha:");
+profit(craftingPrice[1], orehaRevenue);
+if (conversion[1]) {
+  console.log("Conversion Green to Blue is Better");
+}
+console.log("");
+
+console.log("Excavating Abidos:");
+profit(craftingPrice[2], abidosRevenue);
+if (conversion[2]) {
+  console.log("Conversion Green to Blue is Better");
+}
+console.log("");
+
+console.log("Foraging Abidos:");
+profit(craftingPrice[3], abidosRevenue);
+if (conversion[3]) {
+  console.log("Conversion Green to Blue is Better");
+}
+console.log("");
+
+console.log("Logging Abidos:");
+profit(craftingPrice[4], abidosRevenue);
+if (conversion[4]) {
+  console.log("Conversion Green to Blue is Better");
+}
+console.log("");
+
+console.log("Mining Abidos:");
+profit(craftingPrice[5], abidosRevenue);
+if (conversion[5]) {
+  console.log("Conversion Green to Blue is Better");
+}
+console.log("");
+
+console.log("Hunting Abidos:");
+profit(craftingPrice[6], abidosRevenue);
+if (conversion[6]) {
+  console.log("Conversion Green to Blue is Better");
+}
+console.log("");
+
 
